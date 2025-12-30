d5180bbd38fd8a6a26dd70a1e4c51dd11502d9b0
Fabrice Krebs <fabrice.krebs@microsoft.com>
2025-12-30 12:28:36 +0100

    Fixed wrong import

--- demo/urls.py
+++ demo/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from demoapp.views import home
 
 urlpatterns = [
     path("", home, name="home"),
