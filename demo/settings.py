06bbf687d216edf94f87b1a26d12598c2c561343
Fabrice Krebs <fabrice.krebs@microsoft.com>
2025-12-30 12:15:47 +0100

    Modified ALLOWED_HOSTS

--- demo/settings.py
+++ demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-#df7&&c&agir1n71^-9elx$(1pm&y1z39+j^t*m(p*^1up0wc8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
